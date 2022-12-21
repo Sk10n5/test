6f5a5f796de5ecd2ee07f06bd59dd2dd4af58e28
Dj Sk10 <120589577+Sk10n5@users.noreply.github.com>
2022-12-21 10:32:54 +0200

    Add files via upload

---
+++ firstTest.js
@@ -0,0 +1,24 @@
+const {Builder} = require ("selenium-webdriver");
+
+async function example(){
+
+//launch the browser
+let driver = await new Builder().forBrowser("Opera").build(); 
+
+
+
+
+//navigate to our application
+await driver.get("https://lambdatest.github.io/sample-todo-app/")
+
+
+
+//add a todo
+
+//close the browser
+
+
+
+
+}
+example()
